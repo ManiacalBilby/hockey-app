08e8110fe6ca595c453e9444a145dcc64e8f3c95
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-04 17:34:15 -0500

    setups seed.js with one new user for testing

---
+++ db/seeds.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const User = require('./models/User')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.once('open', () => {
+    console.log('Mongoose has connected to MongoDB')
+})
+
+mongoose.connection.on('error', (error) => {
+    console.error(`MongoDB connection error! ${error}`)
+    process.exit(-1)
+})
+
+
+
+User.remove(({})).then(() => {
+    const davidHanson = new User ({
+        firstname: 'David',
+        lastName: 'Hanson',
+    })
+    return davidHanson.save()
+})
+.then(() => {
+    mongoose.connection.close()
+    console.log(`Finished seeding database...
+    
+    Disconnected from MongoDB
+    `)
+})
