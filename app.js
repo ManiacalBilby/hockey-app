28199d05b5a7a993dd3698305b64fca53d8b5fc9
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-05 12:33:56 -0500

    added mongoose connection to app.js

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI)
 
+mongoose.connection.once('open', () => {
+  console.log('Mongoose has connected to MongoDB')
+})
+
+mongoose.connection.on('error', (error) => {
+  console.error(`MongoDB connection error! ${error}`)
+  process.exit(-1)
+})
+
 const userController = require('./routes/userController')
 
 const app = express()
