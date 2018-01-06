44dfe3df42fb206138a0bae0db24e81b32d49f07
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-05 19:15:12 -0500

    added GET route for edit user

--- routes/userController.js
+++ routes/userController.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const User = require('../db/models/User')
 
 /* GET users listing. */
-router.get('/', function(request, response, next) {
+router.get('/', function (request, response, next) {
   User.find({})
     .then((users) => {
       response.render('users/index', {
@@ -24,27 +24,45 @@ router.post('/', (request, response) => {
   const newUser = request.body
 
   User.create(newUser)
-  .then(() => {
-    response.redirect('/users')
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+    .then(() => {
+      response.redirect('/users')
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 
 
 router.get('/:userId', (request, response) => {
   const userId = request.params.userId
+
   User.findById(userId)
     .then((user) => {
       response.render('users/show', {
         user,
         pageTitle: user.firstName
       })
-      .catch((error) => {
-        console.log(error)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
+
+
+router.get('/:userId/edit', (request, response) => {
+  const userId = request.params.userId
+
+  User.findById(userId)
+    .then((user) => {
+      response.render('users/edit', {
+        user,
+        pageTitle: 'Update Profile'
       })
+    })
+    .catch((error) => {
+      console.log(error)
     })
 })
 
