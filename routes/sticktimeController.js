6f4d9a6bee227c9366384feee0b9bed72cadd37a
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-07 18:42:31 -0500

    module.exports

--- routes/sticktimeController.js
+++ routes/sticktimeController.js
@@ -2,11 +2,10 @@ const express = require('express')
 const router = express.Router({ mergeParams: true })
 const User = require('../db/models/User')
 
-router.get('/sticktimeId', (request, response) => {
+router.get('/:sticktimeId', (request, response) => {
     const userId = request.params.userId
     const storeId = request.params.rinkId
     const sticktimeId = request.params.sticktimeId
-})
 
 User.findById(userId)
     .then((user) => {
@@ -24,3 +23,5 @@ User.findById(userId)
         console.log(error)
     })
 })
+
+module.exports = router
